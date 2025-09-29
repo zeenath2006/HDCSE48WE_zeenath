11f9cda070e90d19d272e8cdc61e2e3431415444
zeenath2006 <zeenathmariyam2006@gmail.com>
2025-09-29 10:36:57 +0530

    initial commit

---
+++ 2025_09_25/2025_09_25/Javascript/js/array.js
@@ -0,0 +1,16 @@
+const cars = ["Saab", "Volvo", "BMW"];
+
+
+const names = [];
+names[0] = "Navod";
+names[1] = "Kasun";
+names[2] = "Amal";
+names[3] = "Thiran";
+names[4] = "Ishan";
+
+let i =0;
+ while(i<names.length){
+    console.log("Names Are :- ",names[i]);
+ }
+
+console.log("Check the type:-",typeof(cars))
---
+++ 2025_09_25/2025_09_25/Javascript/js/index.js
@@ -0,0 +1,12 @@
+let name = "Navod";
+let age = 30;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+
+console.log("Check Type \n", typeof(name));
+console.log("Check Type \n", typeof(age));
+console.log("Check Type \n", typeof(isApproved));
+console.log("Check Type \n", typeof(firstName));
+console.log("Check Type \n", typeof(selectedColor));
